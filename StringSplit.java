760e0d3f36f6cae25c1080673921c94e1b30d028
fotisPag <fotis.pagonis01@gmail.com>
2024-04-03 23:53:25 +0300

    StringSplit

---
+++ StringSplit.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch7;
+
+/*
+* Διασπά το String σε επιμέρους substrings(tokens) και επιστρέφει ένα
+* πίνακα των επιμερούς tokems
+*
+* */
+
+
+public class StringSplit {
+    public static void main(String[] args) {
+        String s = "Athens University of Economics and Business";
+
+        String[] tokens = s.split(" ");
+
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+}
