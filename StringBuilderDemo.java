84a4940ac724e4309427c736186500c37f6e2046
fotisPag <fotis.pagonis01@gmail.com>
2024-04-04 00:03:05 +0300

    StringBuilderDemov3

--- StringBuilderDemo.java
+++ StringBuilderDemo.java
@@ -12,7 +12,7 @@ package gr.aueb.cf.ch7;
 
 public class StringBuilderDemo {
     public static void main(String[] args) {
-        String Builder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         String result = "";
         long timeStart, timeEnd;
         double stringConcatTime, stringBuilderConcatTime;
@@ -21,6 +21,15 @@ public class StringBuilderDemo {
         for (int i = 1; i<=50_000; i++){
             result = result + i;
         }
+
+        timeEnd = System.currentTimeMillis();
+        stringConcatTime = (double) (timeEnd - timeStart) / 1000;
+
+        timeStart = System.nanoTime();
+        stringBuilderConcatTime = (double) (timeEnd - timeStart) / 1_000_000;
+
+        System.out.println("Concatenation: String vs StringBuilder time in sec");
+        System.out.println(stringConcatTime + " sec vs " + stringBuilderConcatTime + " sec");
 
 
 
