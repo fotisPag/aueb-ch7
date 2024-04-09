3375f46b9ade913a4dc7188bd91d0b3760cc953e
fotisPag <fotis.pagonis01@gmail.com>
2024-04-09 22:01:02 +0300

    SubstringDemov2

--- SubstringDemo.java
+++ SubstringDemo.java
@@ -11,7 +11,7 @@ public class SubstringDemo {
     public static void main(String[] args) {
         String s = "This is AUEB";
 
-        for(int i=0; i<s.length(); i++){
+        for(int i=0; i < s.length(); i++){
             System.out.print(s.substring(i, i +1));
         }
     }
